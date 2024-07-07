f23b9f78b80d2eb3bb8b8fa3dcf733f0ab499159
Dishan Anuradha <dishananuradha@gmail.com>
2024-07-07 22:40:55 +0530

    Defence.java craeted

---
+++ Defence.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+class Defence {
+    private ArrayList<SuperDefence> defenceList = new ArrayList<SuperDefence>();
+
+    public void addDefence(SuperDefence defence){
+        defenceList.add(defence);
+    }
+}
