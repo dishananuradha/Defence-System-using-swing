dc4f2a59108c500e044d5d00f5203301bb793707
Dishan Anuradha <dishananuradha@gmail.com>
2024-07-08 17:16:44 +0530

    singleton removed

--- Starter.java
+++ Starter.java
@@ -1,12 +1,9 @@
 class Starter{
-    public static MainController mainController = MainController.getInstance();
     public static void main(String[] args){
+        Observer observer = new Observer();
+        MainController mainController = new MainController(observer);
         mainController.setVisible(true);
-        addDefences();
-    }
 
-    public static void addDefences(){
-        Observer observer = mainController.getObserver();
         observer.addDefence(new Helicopter(mainController));
         observer.addDefence(new Tank(mainController));
         observer.addDefence(new Submarine(mainController));
